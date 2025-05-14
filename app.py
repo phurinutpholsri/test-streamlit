a3d77171636fe1cff895d91c7242eb98c8a41888
phurinutpholsri <48591087+phurinutpholsri@users.noreply.github.com>
2025-05-14 20:21:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 import streamlit as st
 from openai import OpenAI
-# Ver 1
 
 with st.sidebar:
-    openai_api_key = st.text_input('Insert OpenAi Key', key="chatbot_api_key", type="password")
+    openai_api_key = st.text_input("Insert OpenAI key", key="chatbot_api_key", type="password")
     "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
     "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
     "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
